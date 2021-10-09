06d71303020ede43b28c0b2bd02113994d1713ac
Justin Maliwat <74742561+maliwat08@users.noreply.github.com>
2021-10-08 23:46:02 -0400

    Check duplicate , expanding

--- GAME2001_A1_MaliwatJustin/Array.h
+++ GAME2001_A1_MaliwatJustin/Array.h
@@ -5,14 +5,17 @@ template<class T>
 class Array
 {
 public:
-	T* m_array;	// Pointer to the beginning of the array
-	// Constructor
-	Array() :
-		m_array(NULL), m_maxSize(0), m_growSize(0), m_numElements(0)
+	Array(int size, int growBy = 1) : m_array(NULL), m_maxSize(0), m_growSize(0), m_numElements(0), m_duplicateData(false)
 	{
-		
+		if (size >= 0)
+		{
+			m_maxSize = size;
+			m_array = new T[m_maxSize];
+			memset(m_array, 0, sizeof(T) * m_maxSize);
+
+			m_growSize = ((growBy > 0)) ? growBy : 0;
+		}
 	}
-	// Destructor
 	~Array()
 	{
 		if (m_array != nullptr)
@@ -21,115 +24,82 @@ public:
 			m_array = nullptr;
 		}
 	}
-
-	// Deletion (2 ways)
-	// Remove the last item inserted into the array
 	void pop()
 	{
 		if (m_numElements > 0)
 		{
-			m_numElements--;	// Reduce the total number of elements by 1. Ignoring the last element.
+			m_numElements--;
 		}
 	}
-	// Remove an item given an index
-	// (Almost) Brute-force execution -- Big-O = O(N)
 	void remove(int index)
 	{
 		assert(m_array != nullptr);
-
 		if (index >= m_numElements)
 		{
-			// I am trying to remove something outside of the bounds of the array
-			return;	// <-- Maybe could do some form of exception handling
-		}
 
+			return;
+		}
 		for (int i = index; i < m_numElements; i++)
 		{
-			// Start at the index we want to remove.
-			// Shift everything after index back by one.
-			if (i + 1 < m_numElements)	// Confines the loop into the array
+			if (i + 1 < m_numElements)
 			{
 				m_array[i] = m_array[i + 1];
 			}
 		}
 		m_numElements--;
 	}
-
-	// Overloaded [] operator
 	T& operator[](int index)
 	{
 		assert(m_array != nullptr && index < m_numElements);
 		return m_array[index];
 	}
-	// Clear
 	void clear()
 	{
-		m_numElements = 0;	 // Ignore (or forgets) all current items in the array
+		m_numElements = 0;
 	}
-	// Gets and Sets
-	int GetSize()
+	int getSize()
 	{
 		return m_numElements;
 	}
-	void SetSize(int n)
-	{
-		m_numElements = n;
-	}
-	int GetMaxSize()
+	int getMaxSize()
 	{
 		return m_maxSize;
 	}
-	void SetMaxSize(int m) {
-		m_maxSize = m;
-	}
-	int GetGrowSize()
+	int getGrowSize()
 	{
 		return m_growSize;
 	}
-	void SetGrowSize(int val)
+	void setGrowSize(int val)
 	{
 		assert(val >= 0);
 		m_growSize = val;
 	}
-	void IncreamentSize()
+	void setDuplicateData(bool x)
 	{
-		m_numElements++;
+		m_duplicateData = x;
 	}
-
-
-	// Expansion
+protected:
 	bool Expand()
 	{
 		if (m_growSize <= 0)
 		{
-			// LEAVE!
 			return false;
 		}
-
-		// Create the new array
 		T* temp = new T[m_maxSize + m_growSize];
 		assert(temp != nullptr);
-
-		// Copy the contents of the original array into the new array
 		memcpy(temp, m_array, sizeof(T) * m_maxSize);
 
-		// Delete the old array
 		delete[] m_array;
-
-		// Clean up variable assignments
 		m_array = temp;
 		temp = nullptr;
-
 		m_maxSize += m_growSize;
-		//expand by 2,4,8,16 
 		m_growSize *= 2;
 		return true;
 	}
-
-
-private:
-
-	int m_maxSize;		// Maximum size of the array
-	int m_growSize;		// Amount the array can grow through expansion
-	int m_numElements;	// Number of items currently in my array
+protected:
+	T* m_array;
+	int m_maxSize;
+	int m_growSize;
+	int m_numElements;
+	bool m_duplicateData;
 };
--- GAME2001_A1_MaliwatJustin/Main.cpp
+++ GAME2001_A1_MaliwatJustin/Main.cpp
@@ -3,3 +3,58 @@
 #include "OrderedArray.h"
 #include "UnorderedArray.h"
 using namespace std;
+
+int main()
+{
+
+	cout << "Unordered Int Array" << endl;
+	UnorderedArray<int> array(5);
+	OrderedArray<int> arraydup(5);
+
+	array.push(69);
+	array.push(31);
+	array.push(94);
+	array.push(55);
+	array.push(64);
+
+	for (int i = 0; i < array.getSize(); i++)
+	{
+		cout << array[i] << ", ";
+	}
+	cout << endl;
+	cout << endl;
+	cout << "Check if there's any duplicate data" << endl;
+	cout << endl;
+	arraydup.setDuplicateData(true);
+
+
+	cout << "Ordered Int Array" << endl;
+	arraydup.push(5);
+	arraydup.push(94);
+	arraydup.push(15);
+	arraydup.push(31);
+	arraydup.push(69);
+
+	for (int i = 0; i < arraydup.getSize(); i++)
+	{
+		cout << arraydup[i] << ",";
+	}
+	arraydup.push(94);
+
+
+	cout << endl;
+	cout << "Expanding" << endl;
+	array.push(2);
+	cout << array.getGrowSize() << endl;
+	array.push(4);
+	cout << array.getGrowSize() << endl;
+	
+
+
+
+
+
+	
+
+
+}
